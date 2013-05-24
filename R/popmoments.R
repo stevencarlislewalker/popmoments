1b581d097305f02578590bf252d6c0e8e93fc343
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-24 22:55:09 +0000

    added functions to popmoments
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/multitable@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/popmoments.R
+++ R/popmoments.R
@@ -41,6 +41,40 @@ E. <- function(x, p){
 #' @export
 var. <- function(x, p) E.(x^2, p) - (E.(x, p)^2)
 
+#' Population-level standard deviation
+#'
+#' Standard deviation of a vector giving the elements of a population.
+#'
+#' @param x A numeric vector giving the population.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The standard deviation of \code{x}.
+#' @export
+sd. <- function(x, p) sqrt(E.(x^2, p) - (E.(x, p)^2))
+
+#' Population-level z-score
+#'
+#' Transform a vector into z-scores using population mean and standard deviation.
+#'
+#' @param x A numeric vector giving the population.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The z-scores of \code{x}.
+#' @export
+z. <- function(x, p) dev.(x, p)/sd.(x, p)
+
+#' Population-level deviation
+#'
+#' Deviations of the elements of a vector from their population mean.
+#'
+#' @param x A numeric vector giving the population.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The deviations of \code{x}.
+#' @export
+dev. <- function(x, p) x - E.(x, p)
+
+
 #' Population-level covariance
 #'
 #' Covariance of two vectors giving the elements of a bivariate
@@ -57,9 +91,9 @@ cov. <- function(x, y, p){
 	return(E.(x*y, p) - (E.(x, p)*E.(y, p)))
 } 
 
-#' Population-level three-variable 'covariance'
+#' Population-level coskewness
 #'
-#' Covariance of three vectors giving the elements of a trivariate
+#' Coskewness of three vectors giving the elements of a trivariate
 #' population.
 #' 
 #' @param x A numeric vector giving the first variable of the population.
@@ -67,15 +101,27 @@ cov. <- function(x, y, p){
 #' @param z A numeric vector giving the third variable of the population.
 #' @param p An optional vector of weights.  If missing, equal 
 #'  weights are used.
-#' @return The 'covariance' of \code{x} and \code{y} and \code{z}.  This 
+#' @return The coskewness of \code{x} and \code{y} and \code{z}.  This 
 #'	is \code{E.((x - E.(x))*(y - E.(y))*(z - E.(z)))}
 #' @export
-cov3. <- function(x, y, z, p){
+coskew. <- function(x, y, z, p){
 	if(length(x) != length(y)) stop('x must be same length as y')
 	if(length(x) != length(z)) stop('x must be same length as z')
 	if(length(y) != length(z)) stop('y must be same length as z')
-	return(E.((x - E.(x))*(y - E.(y))*(z - E.(z))))
+	return(E.(dev.(x, p)*dev.(y, p)*dev.(z, p), p))
 }
+
+#' Population-level skewness
+#'
+#' Skewness of a vector giving the elements of a population.
+#' 
+#' @param x A numeric vector giving the first variable of the population.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The skewness of \code{x}.  This 
+#'  is \code{E.((x - E.(x))^3)}
+#' @export
+skew. <- function(x, p) E.(dev.(x, p)^3, p)
 
 #' Population-level correlation
 #'
@@ -86,7 +132,7 @@ cov3. <- function(x, y, z, p){
 #' @param y A numeric vector giving the second variable of the population.
 #' @param p An optional vector of weights.  If missing, equal 
 #'  weights are used.
-#' @return The correlation of \code{x} \code{y}.
+#' @return The correlation of \code{x} and \code{y}.
 #' @export
 cor. <- function(x, y, p) cov.(x, y, p)/sqrt(var.(x, p)*var.(y, p))
 
@@ -100,6 +146,36 @@ cor. <- function(x, y, p) cov.(x, y, p)/sqrt(var.(x, p)*var.(y, p))
 #'  population.
 #' @param p An optional vector of weights.  If missing, equal 
 #'  weights are used.
-#' @return The correlation of \code{x} \code{y}.
+#' @return The regression of \code{y} and \code{x}.
 #' @export
 reg. <- function(y, x, p) cov.(x, y, p)/var.(x, p)
+
+#' Population-level regression intercept
+#'
+#' Intercept in the regression of one vector on another in a bivariate population.
+#' 
+#' @param x A numeric vector giving the independent variable of 
+#'  the population.
+#' @param y A numeric vector giving the dependent variable of the 
+#'  population.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The intercept associated with \code{x} and \code{y}.
+#' @export
+int. <- function(y, x, p) E.(y, p) - reg.(y, x, p)*E.(x, p)
+
+#' General population-level mixed central moment
+#'
+#' Mixed central moment of an arbitrary number of vectors in a multivariate population.
+#' 
+#' @param df A data frame of numeric vectors of the same length.
+#' @param p An optional vector of weights.  If missing, equal 
+#'  weights are used.
+#' @return The mixed central moment of the variables in \code{df}.
+#' @export
+cm. <- function(df, p){
+  if(missing(p)) p <- rep(1, length(df[[1]]))
+  if(any(length(df[[1]]) != sapply(df, length))) stop('all variables must be same length')
+  df <- sweep(df, 2, apply(df, 2, E., p = p), '-')
+  return(E.(apply(df, 1, prod), p))
+}
